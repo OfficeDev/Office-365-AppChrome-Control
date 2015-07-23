1b494baa7e021061ed3e4e48981797021f2051c7
Brenda Yang <t-fayang@outlook.com>
2015-07-23 16:58:40 +0800

    SPA server side demo solution

---
+++ LoginControlForSPASolution/LoginControlForSPASolution/Controllers/WebAPIController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace LoginControlForSPASolution.Controllers
+{
+    [Authorize]
+    public class WebAPIController : ApiController
+    {
+        // GET api/WebAPI
+        public string Get()
+        {
+            string owner = ClaimsPrincipal.Current.FindFirst(ClaimTypes.Name).Value;
+            return "Hello, Server know you, "+owner+"!";
+        }
+
+        // GET api/WebAPI/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/WebAPI
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/WebAPI/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/WebAPI/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ LoginControlForSPASolution/LoginControlForSPASolution/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(LoginControlForSPASolution.Startup))]
+
+namespace LoginControlForSPASolution
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
