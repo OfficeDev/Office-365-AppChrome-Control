e4b2fb637bbf0801691df4a9ce61cf4a24e2c0a8
whazmy <443146729@qq.com>
2015-08-31 16:58:24 +0800

    add .ts files

---
+++ AppChromeControl-demo/ts/Office.Controls.AppChrome.d.ts
@@ -0,0 +1,14 @@
+﻿declare module Office.Controls {
+    interface LoginConfig {
+        clientId: string;
+        redirectUri: string;
+        postLogoutRedirectUri: string;
+        cacheLocation: string;
+    }
+    class ImplicitGrantLogin {
+        constructor(config: Office.Controls.LoginConfig);
+    }
+    class AppChrome {
+        constructor(appName: string, root: HTMLElement, loginProvider: ImplicitGrantLogin, options);
+    }
+}
---
+++ AppChromeControl-demo/ts/sample.ts
@@ -0,0 +1,10 @@
+﻿var element = document.getElementById("container");
+
+var login = new Office.Controls.ImplicitGrantLogin({
+    clientId: '********-****-****-****-************',
+    redirectUri: window.location.href,
+    postLogoutRedirectUri: window.location.toString(),
+    cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
+});
+
+var appChrome = new Office.Controls.AppChrome("AppName", element, login, {});
