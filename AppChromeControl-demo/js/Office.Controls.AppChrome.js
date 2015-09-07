693055d7e74c8f8c0677469aaef35f8d81a0b1d0
whazmy <443146729@qq.com>
2015-09-07 17:09:15 +0800

    context Menu Separator

--- AppChromeControl-demo/js/Office.Controls.AppChrome.js
+++ AppChromeControl-demo/js/Office.Controls.AppChrome.js
@@ -257,11 +257,16 @@
         innerHtml += '<div class=\"o365cs-base ms-bgc-w o365cst o365cs-context-font o365cs-me-contextMenu\"><div autoid=\"__Microsoft_O365_ShellG2_Plus_templates_cs_0\" class=\"o365cs-me-itemsList\" tabindex=\"-1\" id=\"additem\"><div>';
         innerHtml += Office.Controls.appChromeTemplates.generatePersonaPart();
         if (!Office.Controls.Utils.isNullOrUndefined(appLinks)) {
+            var countItem = 0;
             for (var name in appLinks) {
                 if(isEmpty(name) || isEmpty(appLinks[name])){
                     continue;
                 }
                 innerHtml += Office.Controls.appChromeTemplates.generateAppLinkPart(name, appLinks[name]);
+                countItem +=1;
+            }
+            if(countItem!=0){
+                innerHtml +=Office.Controls.appChromeTemplates.generateMenuSeparator();
             }
         }
         innerHtml += Office.Controls.appChromeTemplates.generateSignOutPart();
@@ -283,9 +288,13 @@
         return innerHtml;
     };
 
-    Office.Controls.appChromeTemplates.generateSignOutPart = function() {
+    Office.Controls.appChromeTemplates.generateMenuSeparator = function() {
         var innerHtml = '<div autoid=\"__Microsoft_O365_ShellG2_Plus_templates_cs_1\" tabindex=\"-1\"><div class=\"o365cs-contextMenuSeparator ms-bcl-nl\"></div></div>';
-        innerHtml += '<div autoid=\"__Microsoft_O365_ShellG2_Plus_templates_cs_1\" class=\"ms-item-tdr\" tabindex=\"-1\" aria-label=\"Sign out and return to the Sign-in page\" title=\"Sign out and return to the Sign-in page\" aria-selected=\"false\"><a class=\"o365button o365cs-contextMenuItem ms-fcl-b\" role=\"link\" id=\"O365_SubLink_ShellSignout\" aria-label=\"Sign out of Office 365 and return to the Sign-in page\">';
+        return innerHtml;
+    }
+
+    Office.Controls.appChromeTemplates.generateSignOutPart = function() {
+        var innerHtml = '<div autoid=\"__Microsoft_O365_ShellG2_Plus_templates_cs_1\" class=\"ms-item-tdr\" tabindex=\"-1\" aria-label=\"Sign out and return to the Sign-in page\" title=\"Sign out and return to the Sign-in page\" aria-selected=\"false\"><a class=\"o365button o365cs-contextMenuItem ms-fcl-b\" role=\"link\" id=\"O365_SubLink_ShellSignout\" aria-label=\"Sign out of Office 365 and return to the Sign-in page\">';
         innerHtml += '<div class=\"_fce_j\"><span class=\"_fce_k owaimg\" role=\"presentation\" style=\"display: none;\"></span><span autoid=\"_fce_4\" aria-label=\"Sign out of Office 365 and return to the Sign-in page\">' + Office.Controls.Utils.htmlEncode(Office.Controls.appChromeResourceString.SignOutString) + '</span></div></a></div>';
         return innerHtml;
     };
